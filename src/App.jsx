c564b783fac0a64ac71c90fc0d5def05c22dff87
elisabichiappini <elisabichiappini@gmail.com>
2026-06-29 18:07:46 +0200

    feat: new accordion

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ export default function () {
   return (
     <div>
       <h1>Hello, World!</h1>
-    
+
       <Accordion title="Accordion Counter">
         <Counter />
       </Accordion>
-
       <Accordion title="Testo Accordion">
         <h2>Contenuto dell'accordion</h2>
+        <p>"Lorem ipsum sorem lorem ipsum ipsum dolor sit amet super lorem ipsum dolor sit amet super lorem ipsum dolor sit amet super" </p>
+      </Accordion>
+      <Accordion title="Breve Accordion">
+        <h2>Brutto testo</h2>
         <p>"Lorem ipsum sorem lorem ipsum ipsum dolor sit amet super lorem ipsum dolor sit amet super lorem ipsum dolor sit amet super" </p>
       </Accordion>
     </div>
