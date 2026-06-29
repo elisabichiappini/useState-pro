1dfb67e747e75617367719f7a381cae2daf11977
elisabichiappini <elisabichiappini@gmail.com>
2026-06-29 18:25:37 +0200

    feat: add solo un accordion aperto

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
+import { useState } from 'react';
 import Counter from './components/Counter.jsx';
 import Accordion from './components/Accordion.jsx';
 
 export default function () {
+  const [currentOpen, setCurrentOpen] = useState(null);
+
   return (
     <div>
       <h1>Hello, World!</h1>
 
-      <Accordion title="Accordion Counter">
+      <Accordion title="Accordion Counter" isOpen={currentOpen === 0} onAccordionClick={() => setCurrentOpen(currentOpen === 0 ? null : 0)} >
         <Counter />
       </Accordion>
-      <Accordion title="Testo Accordion">
+      <Accordion title="Testo Accordion" isOpen={currentOpen === 1} onAccordionClick={() => setCurrentOpen(currentOpen === 1 ? null : 1)} >
         <h2>Contenuto dell'accordion</h2>
         <p>"Lorem ipsum sorem lorem ipsum ipsum dolor sit amet super lorem ipsum dolor sit amet super lorem ipsum dolor sit amet super" </p>
       </Accordion>
-      <Accordion title="Breve Accordion">
+      <Accordion title="Breve Accordion" isOpen={currentOpen === 2} onAccordionClick={() => setCurrentOpen(currentOpen === 2 ? null : 2)} >
         <h2>Brutto testo</h2>
         <p>"Lorem ipsum sorem lorem ipsum ipsum dolor sit amet super lorem ipsum dolor sit amet super lorem ipsum dolor sit amet super" </p>
       </Accordion>
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { IoMdArrowDropupCircle as CloseIcon, IoMdArrowDropdownCircle as OpenIcon } from "react-icons/io";
 
-export default function ({ title, children }) {
-    const [isOpen, setIsOpen] = useState(false);
+export default function ({ title, children, isOpen, onAccordionClick }) {
+    
     return (
         <div className="accordion">
             <section className="title">
                 <h2>{title}</h2>
-                <button onClick={() => setIsOpen(!isOpen)}>
+                <button onClick={onAccordionClick }>
                     {isOpen ? <CloseIcon /> : <OpenIcon />}
                 </button>
             </section>
